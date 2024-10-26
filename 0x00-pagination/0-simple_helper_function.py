5d5d01a3a447a2f9bb83f91fe5c3017761d0d9ae
root <onaperpetua@gmail.com>
2024-10-26 18:28:41 +0000

    commit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,7 +9,8 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """A method that retrives the index range from a given page and page size
     """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    start = (page - 1) * page_size
+    end = start + page_size
 
-    return (start_index, end_index)
+    return (start, end)
+
